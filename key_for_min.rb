436c250e1df1088724bfc7e632d42f13859c7cab
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-17 17:01:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-family = {:marc => 52, :alexis: => 47; :carissa => 36}
-
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_numerator = nil
+    
 end
