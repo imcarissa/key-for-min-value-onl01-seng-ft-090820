b7c831f20f08bf3f4392fdee4f1478771404b254
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-17 17:04:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_key = nil
+  lowest_integer = nil
   lowest_numerator = nil
     name_hash.each do |
 end
