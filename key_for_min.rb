93a8ecd423e7c883c9c53ad46102811343167e10
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-17 17:08:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_integer = nil
   lowest_numerator = nil
-    name_hash.each do |a, b|
+    name_hash.each do |i, n|
       if lowest_integer == nil || b < lowest_integer
       lowest_integer = b 
       lowest_numerator = a
